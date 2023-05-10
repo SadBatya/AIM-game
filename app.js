8021277234ee389f41e88cbd32bab9deb6268d38
sadbatya <sadbatya@mail.ru>
2023-05-10 22:30:15 +0200

    happy color

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const screens = document.querySelectorAll('.screen');
 const timeList = document.querySelector('#time-list');
 const timeEl = document.querySelector('#time')
 const board = document.querySelector('#board')
+const colors = ['Aqua', 'Yellow', 'Lavender', 
+                'Navy','SlateBlue','Honeydew','Maroon','Gray',
+                'Gainsboro','Black','Fuchsia','Purple']
 let time = 0;
 let score = 0;
 
@@ -55,14 +58,20 @@ function finishGame(){
   board.innerHTML = `<h1>Cчет: <span class='primary'>${score}</span></h1>`
 }
 
+function getRandomColor(){
+  const index = Math.floor(Math.random() * colors.length)
+  return colors[index]
+}
 
 function createRandomCircle(){
   const circle = document.createElement('div')
   const size = getRandomNumber(10, 60)
+  const color = getRandomColor()
   const {width, height} = board.getBoundingClientRect()
   const x = getRandomNumber(0, width - size)
   const y = getRandomNumber(0, height - size)
   circle.classList.add('circle')
+  circle.style.background = color
   circle.style.width = `${size}px`
   circle.style.height = `${size}px`
   circle.style.top = `${y}px`
